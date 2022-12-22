89b37356ef35e3c8ca79d161bb337f7ef4cdd052
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-12-21 21:30:12 -0500

    functioning with useEffect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import MovieDisplay from './components/MovieDisplay';
 import Form from './components/Form';
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 function App() {
   // API Key
@@ -18,7 +18,11 @@ function App() {
     // update the movie state
     setMovie(data)
   }
-
+  console.log("log in the body of the component")
+  useEffect(()=> {
+    getMovie("Titanic")
+    console.log("log in the use effect")
+  }, [])
   return (
 
     <div className="App">
--- src/components/MovieDisplay.js
+++ src/components/MovieDisplay.js
@@ -2,10 +2,18 @@
 import React from "react";
 
 // Define a function that is our component, always make sure to declare the props parameter so you can use props in your component
-const MovieDisplay = (props) => {
-    //The component must return some JSX
-    return <h1>The MovieDisplay Component</h1>;
-  };
+const MovieDisplay = ({movie}) => {
+    const loaded = () => {
+     return <>
+    <h1>{movie?.Title}</h1>
+    <h2>{movie?.Genre}</h2>
+    <img src={movie?.Poster} alt={movie?.Title}/>
+    <h2>{movie?.Year}</h2>
+    </>}
+    const loading = () => <h1>No Movie to Display</h1>
+    // expression ? true : false
+    return movie ? loaded() : loading()
+};
   
   // We must export the component to use it in other files
   export default MovieDisplay;
